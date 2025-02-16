6c75de0773b6d7230a1aa84333b4be1bbdc28214
Mayankkamriya <kamriyamayank45@gmail.com>
2025-02-16 16:52:58 +0530

    add env port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var cors =require('cors');
 connectToMongo();
 
 const app = express();
-const port = 5000;  // Because he used 3000 in chrome web and 5000 Thunderclient
+const port =process.env.PORT || 5000;  // Because he used 3000 in chrome web and 5000 Thunderclient
 
 app.use(cors());
 app.use(express.json());// if we want to use 'req.body' then we need to use middleware which is 'express.json'. After writing this we can deal in json. we can send request in json
